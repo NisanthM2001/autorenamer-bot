ca7c43a06dce7da60450161cae9a41d04d356256
NisanthM2001 <104443364+NisanthM2001@users.noreply.github.com>
2025-12-01 16:30:33 +0530

    Ultra Simple: Remove sessions, database dependencies

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,10 @@
+"""
+Pyrogram Bot Client - Ultra Simple
+"""
 from pyrogram.client import Client
 from bot.config import Config
 
+# Bot client only
 app = Client(
     "bot_session",
     api_id=Config.API_ID,
@@ -8,13 +12,3 @@ app = Client(
     bot_token=Config.BOT_TOKEN,
     workdir="."
 )
-
-user_client = None
-if Config.SESSION_STRING:
-    user_client = Client(
-        "user_session",
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        session_string=Config.SESSION_STRING,
-        workdir="."
-    )
